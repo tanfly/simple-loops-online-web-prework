9d76347fd68117285af2ece11bd584e86c656a88
Stella McEwen <mcewen.stella@gmail.com>
2019-04-07 20:32:33 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -22,7 +22,7 @@ end
 def while_iterator(number_of_times)
   counter = 0 
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  while counter <= 7
+  while counter =< 7
   counter += 1 
   puts phrase
 end
