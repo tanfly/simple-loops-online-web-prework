b165eda96ef6398c871651e6c923be594f364c0e
Stella McEwen <mcewen.stella@gmail.com>
2019-04-07 20:38:24 +0000

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,42 +1,47 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
-  # Code your solution here using the "loop" keyword to puts out the below phrase
-  # This and all the other methods take an argument of an integer 
-  # The integer is the number of times the loops should puts out the phrase
-  # How can we make sure the loop breaks once it has puts out the phrase the 
-  #   correct number of times?
-  # Maybe we should keep count of the number of times we've puts out the 
-  #  phrase and break when the counter hits the appropriate number...
+  counter = 0 
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  loop do 
+    counter += 1 
+  puts phrase
+  if counter >= 7
+    break
+end
+end
 end
 
 def times_iterator(number_of_times)
-  # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  7.times do
+    puts phrase
+  end
 end
 
 def while_iterator(number_of_times)
-  # code your solution here using the "while" keyword
-  # hint: user a counter to tell the while loop when to stop!
+  counter = 0 
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  while counter < 7
+  puts phrase
+  counter += 1 
+end
 end
 
 def until_iterator(number_of_times)
-  # code your solution here using the "until" keyword
-  # hint: use a counter to tell the until loop when to stop!
+ counter = 0 
   phrase = "Welcome to Flatiron School's Web Development Course!"
-
+  until counter == 7
+  puts phrase
+  counter += 1
+end
 end
 
 def for_iterator(number_of_times)
-  # code your solution here using the "for" keyword
-  # remember that `for` requires a range of numbers. How can we turn our number into a range?
-  
+  welcome_count = 1..7
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  for welcome in welcome_count
+  puts phrase
+end
 end
 
